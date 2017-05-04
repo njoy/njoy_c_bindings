d5af9088be24a81e1c5b049dad95050e7c9fa3cf
Travis CI <travis@travis-ci.org>
2017-05-04 16:01:20 -0600

    This takes forever.

--- .travis/signature.sh
+++ .travis/signature.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 if [ "$TRAVIS_PULL_REQUEST" = false ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
